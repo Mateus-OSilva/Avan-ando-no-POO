d0d58ab8f8317a118ed566f1ee2fa0eb1e3c5587
Mateus-OSilva <mateusods20@gmail.com>
2022-11-21 19:56:48 -0300

    metódo like

--- modelo.py
+++ modelo.py
@@ -1,20 +1,28 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+        
+    def dar_like(self):
+        self.likes +=1
         
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+        
+    def dar_like(self):
+        self.likes +=1
         
 vingadores = Filme('Vingadores - guerra infinita', 2018, 160)
-print(vingadores.nome)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} ' 
+      f'- Temporadas: {vingadores.temporadas} - Likes: {vingadores.likes}')
 
 atlanta = Serie('Atlanta', 2017, 2)
 
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} ' 
-      f'- Temporadas: {atlanta.temporadas}')
-teste
+      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
